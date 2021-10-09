c97e1edd28f052c2a1c48bfbf17b7442d529fb64
Andrey Leshchiner <al9999things@gmail.com>
2021-10-09 19:32:21 -0400

    String Parsing

--- AlgebraLineArt.py
+++ AlgebraLineArt.py
@@ -1,5 +1,6 @@
 pointList = []
 lineList = []
+use2Functions = False
 
 def ReplaceABCD(a, b, c, d, string):
     string = string.replace("%a%", a)
@@ -24,25 +25,34 @@ def CreateLinesFromString(lineString):
     for line in lineStrings:
         points.append(str(line).split("_"))
     for point in points:
-        startPoint = str(point[0]).split(",")
-        endPoint = str(point[1]).split(",")
-        CreateLine(CreatePoint(int(startPoint[0]), int(startPoint[1])), CreatePoint(int(endPoint[0]), int(endPoint[1])))
+        pointA = str(point[0]).split(",")
+        pointB = str(point[1]).split(",")
+        CreateLine(CreatePoint(pointA[0], pointA[1]), CreatePoint(pointB[0], pointB[1]))
         
 class Point:
     def __init__(self, x, y, pointID):
         self.x = x
         self.y = y
         self.pointID = pointID
+    
+    def GetLefter(self, point): return self.x if self.x >= point.x else point.x
+    def GetRighter(self, point): return self.x if self.x < point.x else point.x
+
 
 class Line:
-     def __init__(self, startPoint, endPoint, lineID):
-        self.startPoint = startPoint
-        self.endPoint = endPoint
+    def __init__(self, pointA: Point, pointB: Point, lineID):
+        self.startPoint = pointA.GetLefter(pointB)
+        self.endPoint = pointA.GetRighter(pointB)
         self.lineID = lineID
-        lineFunctionA = ReplaceABCD(str(startPoint.x), str(startPoint.y), str(endPoint.x), str(endPoint.y), "y-%b%=\\left\\{%a%<%c%:\\frac{\\left(%d%-%b%\\right)}{%c%-%a%}\\left(x-%a%\\right)\\left\\{%a%\\le x\\le %c%\\right\\}\\right\\}")
-        lineFunctionB = ReplaceABCD(str(startPoint.x), str(startPoint.y), str(endPoint.x), str(endPoint.y), "y-%b%=\\left\\{%c%<%a%:\\frac{\\left(%d%-%b%\\right)}{%c%-%a%}\\left(x-%a%\\right)\\left\\{%c%\\le x\\le %a%\\right\\}\\right\\}")
-        print(lineFunctionA)
-        print(lineFunctionB)
+        global use2Functions
+        if use2Functions:
+            lineFunctionA = ReplaceABCD(str(self.startPoint.x), str(self.startPoint.y), str(self.endPoint.x), str(self.endPoint.y), "y-%b%=\\left\\{%a%<%c%:\\frac{\\left(%d%-%b%\\right)}{%c%-%a%}\\left(x-%a%\\right)\\left\\{%a%\\le x\\le %c%\\right\\}\\right\\}")
+            lineFunctionB = ReplaceABCD(str(self.startPoint.x), str(self.startPoint.y), str(self.endPoint.x), str(self.endPoint.y), "y-%b%=\\left\\{%c%<%a%:\\frac{\\left(%d%-%b%\\right)}{%c%-%a%}\\left(x-%a%\\right)\\left\\{%c%\\le x\\le %a%\\right\\}\\right\\}")
+            print(lineFunctionA)
+            print(lineFunctionB)
+        else:
+            lineFunction = ReplaceABCD(str(self.startPoint.x), str(self.startPoint.y), str(self.endPoint.x), str(self.endPoint.y), "y-%b%=\\frac{\\left(%d%-%b%\\right)}{%c%-%a%}\\left(x-%a%\\right)\\left\\{%a%\\le x\\le %c%\\right\\}")
+            print(lineFunction)
 
 testLineString = "|0,0_1,1|-1,1_4,3|"
 CreateLinesFromString(testLineString)
